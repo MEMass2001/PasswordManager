ae4c83618f747842a0937940062ef6c34ec87251
Kesho <sawin.world@gmail.com>
2022-05-13 11:49:45 +0500

    Colors and icon

--- PasswordManagerApp.Android/Properties/AssemblyInfo.cs
+++ PasswordManagerApp.Android/Properties/AssemblyInfo.cs
@@ -6,6 +6,9 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
+[assembly: Application(Icon = "@drawable/AppIcon")]
+
 [assembly: AssemblyTitle("PasswordManagerApp.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
